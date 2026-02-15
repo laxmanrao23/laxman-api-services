9bf5a3f7c5945a2fc32eac468451cecc2893a131
Laxman Rao Savirigapu <109236954+laxmanrao23@users.noreply.github.com>
2026-02-15 05:55:19 +0530

    Added Cart Service and Two DBs Monogo and PostgreSQL

--- src/main/java/com/laxman/job/server/JobListingApplication.java
+++ src/main/java/com/laxman/job/server/JobListingApplication.java
@@ -2,10 +2,26 @@ package com.laxman.job.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+
+// 👉 JPA → ONLY CART (PostgreSQL)
+@EnableJpaRepositories(
+        basePackages = "com.laxman.job.server.cart.repository"
+)
+
+// 👉 MongoDB → USERS + PRODUCTS + JOBS
+@EnableMongoRepositories(
+        basePackages = {
+                "com.laxman.job.server.repository",              // users
+                "com.laxman.job.server.product.repository"       // products
+        }
+)
+
 public class JobListingApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobListingApplication.class, args);
